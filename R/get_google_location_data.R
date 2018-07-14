684a912b3c625fe6f324464729dfd846a55c4eb2
jruwaard <jruwaard@me.com>
2018-07-14 02:07:51 +0200

    document function

--- R/get_google_location_data.R
+++ R/get_google_location_data.R
@@ -1,12 +1,12 @@
-#' Import Google GPS Location TimeLine
+#' Import Google Location Location history
 #'
-#' Import a JSON Google GPS Location History into R.
+#' Import a Google Location History JSON file into R.
 #'
 #' @param file a ".json" file, containing a Google location history.
 #' @return a \code{data.frame}, with variables:
 #'   \itemize{
 #'     \item{id}{Participant identifier}
-#'     \item{TimeStamp}{Datetime of measurement (POSICxt)}
+#'     \item{timestamp}{Datetime of measurement (POSICxt)}
 #'     \item{lat}{GPS Latitude}
 #'     \item{lon}{GPS Longitude}
 #'     \item{accuracy}{Accuracy of location (in meters)}
@@ -15,10 +15,13 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom readr read_file
 #'
+#' @seealso https://takeout.google.com/settings/takeout/custom/location_history
+#' (to export your location history into JSON); \code{\link{locations}} for a
+#' data set containing four-week location histories of two people.
+#'
 #' @export
 #'
 #' @examples
-#'
 #' example_json <- system.file("extdata", "google_timeline_sample.json",
 #'                            package = "emaph")
 #' d <- get_google_location_data(example_json)
@@ -31,8 +34,8 @@ get_google_location_data <- function(file = NULL) {
   gdat <- jsonlite::fromJSON(jsondata)
   locs <- gdat$locations
 
-  d <- data.frame(TimeStamp = as.numeric(locs$timestampMs) / 1000)
-  d$TimeStamp <- as.POSIXct(d$TimeStamp, origin = "1970-01-01")
+  d <- data.frame(timeStamp = as.numeric(locs$timestampMs) / 1000)
+  d$timeStamp <- as.POSIXct(d$timeStamp, origin = "1970-01-01")
 
   d$lat <- locs$latitudeE7 / 1e7   # E7 -> GPS
   d$lon <- locs$longitudeE7 / 1e7
